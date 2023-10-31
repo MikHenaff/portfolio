f145da3c9af0fc02ed91e6f14c8dbd342005fe69
mikhenaff <109875471+mikhenaff@users.noreply.github.com>
2023-10-31 16:44:30 +0100

    fix paths for deployment

--- src/data/projectsDB.js
+++ src/data/projectsDB.js
@@ -1,5 +1,5 @@
-//import TraineeProjectHomepage from "../assets/img/projects/projet-formation-homepage.png";
-//import TraineeProjectContent from "../assets/img/projects/projet-formation-collection.png";
+import TraineeProjectHomepage from "../assets/img/projects/projet-formation-homepage.png";
+import TraineeProjectContent from "../assets/img/projects/projet-formation-collection.png";
 //import CocktailTimeHomepage from "../assets/img/projects/cocktail-time-homepage.png";
 //import CocktailTimeContent from "../assets/img/projects/cocktail-time-content.png";
 //import KaamelottContent from "../assets/img/projects/kaamelott-content.png";
@@ -10,8 +10,10 @@ export const projectsDB = [
     name: "Trainee Project",
     //img1src: "./src/assets/img/projects/projet-formation-homepage.png",
     //img2src: "../src/assets/img/projects/projet-formation-collection.png",
-    img1src: "./assets/img/projects/projet-formation-homepage.png",
-    img2src: "../assets/img/projects/projet-formation-collection.png",
+    //img1src: "./assets/img/projects/projet-formation-homepage.png",
+    //img2src: "../assets/img/projects/projet-formation-collection.png",
+    img1src: { TraineeProjectHomepage },
+    img2src: { TraineeProjectContent },
     technos: ["- HTML/CSS", "- JavaScript", "- PHP", "- MySQL"],
     desc: 'A movie application based on "The Movie Database" <a href="https://www.themoviedb.org" rel="noreferrer" target="_blank" class="text-slate-200 underline hover:no-underline">(themoviedb.org)</a>.<br/>The user can create an account and collect information about the movies in their video library, stay updated on the latest, most popular films, or those coming soon to theaters, and search for information by movie title.',
     codeLink: "https://github.com/mikhenaff/MonCineClub",
