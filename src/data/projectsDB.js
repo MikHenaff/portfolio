965097cdc1e6c0d7afe325e3b5a3a30cb36a94f2
mikhenaff <109875471+mikhenaff@users.noreply.github.com>
2023-10-31 16:39:18 +0100

    fix paths for deployment

--- src/data/projectsDB.js
+++ src/data/projectsDB.js
@@ -8,7 +8,7 @@
 export const projectsDB = [
   {
     name: "Trainee Project",
-    img1src: "../assets/img/projects/projet-formation-homepage.png",
+    img1src: "./src/assets/img/projects/projet-formation-homepage.png",
     img2src: "../src/assets/img/projects/projet-formation-collection.png",
     technos: ["- HTML/CSS", "- JavaScript", "- PHP", "- MySQL"],
     desc: 'A movie application based on "The Movie Database" <a href="https://www.themoviedb.org" rel="noreferrer" target="_blank" class="text-slate-200 underline hover:no-underline">(themoviedb.org)</a>.<br/>The user can create an account and collect information about the movies in their video library, stay updated on the latest, most popular films, or those coming soon to theaters, and search for information by movie title.',
